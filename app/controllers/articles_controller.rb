a8d712f97cae3a0926f1cce6bbf5be110b6bacb1
Sadie Anderson <sander94@uncc.edu>
2019-11-06 02:26:52 +0000

    Add validations for title

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,7 +11,7 @@ class ArticlesController < ApplicationController
     end 
 
     def create 
-        @article = Article.new(article_params)
+        @article = Article.new(article_path)
         
         @article.save
         redirect_to @article
