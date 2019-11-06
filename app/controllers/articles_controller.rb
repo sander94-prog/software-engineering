3cf12a48c7ae419949a167ec3cd5931461a7946b
Sadie Anderson <sander94@uncc.edu>
2019-11-06 02:58:08 +0000

    Add text validations and error messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,22 +3,24 @@ class ArticlesController < ApplicationController
         @articles = Article.all
     end
     
-    def show
-        @article = Article.find(params[:id])
-    end
-    
     def new
         @article = Article.new
     end 
 
     def create 
-        @article = Article.new(article_param)
-        
+        @article = Article.new(article_params)
     if @article.save
         redirect_to @article
     else
         render 'new'
     end
-        
-    end 
+    end
+    def show
+        @article = Article.find(params[:id])
+    end
+    
+    private
+    def article_params
+        params.require(:article).permit(:title, :description)
+    end
 end
