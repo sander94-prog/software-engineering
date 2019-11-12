c853c29e1c3254ed464a769805b4065184d0abef
Sadie Anderson <sander94@uncc.edu>
2019-11-12 21:52:54 +0000

    Add destroy functionality for comments

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,20 +3,22 @@ class ArticlesController < ApplicationController
         @articles = Article.all
     end
     
+    def show
+        @article = Article.find(params[:id])
+    end
+
     def new
         @article = Article.new
     end 
 
     def create 
         @article = Article.new(article_params)
-        byebug
         
-    if @article.save
-        byebug
-        redirect_to @article
-    else
-        render 'new'
-    end
+        if @article.save
+            redirect_to @article
+        else
+            render 'new'
+        end
     end
     
     def edit
@@ -25,26 +27,23 @@ class ArticlesController < ApplicationController
     
     def update
         @article = Article.find(params[:id]) 
+        
         if @article.update(article_params)
             redirect_to @article 
         else
             render 'edit' 
         end
     end
-    
     def destroy
-        @article = Article.find(params[:id]) 
+        @article = Article.find(params[:id])
         @article.destroy
         
-        redirect_to articles_path 
-    end
-
-    def show
-        @article = Article.find(params[:id])
-    end
-    
-    private
-    def article_params
-        params.require(:article).permit(:title, :description)
+        redirect_to articles_path
     end
 end
+
+private
+    def article_params
+        params.require(:article).permit(:title, :text)
+    end
+    
